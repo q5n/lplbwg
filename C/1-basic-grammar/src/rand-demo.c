74173f5d55a8a07fa8cb67c1eb1421fa3dcc4758
Quentin Liu <quentnliu@gmail.com>
2021-07-30 22:06:49 +0800

    test rand function

---
+++ C/1-basic-grammar/src/rand-demo.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+
+int main(void){
+    int min=rand();
+    int max=min;
+    for (int i=0;i<100000;i++){
+        int a=rand();
+        if(a>max){
+            max=a;
+        }
+        if(a<min){
+            min=a;
+        }
+    }
+    printf("rand min:%d, max:%d\n",min,max);
+    printf("RAND_MAX:%d\n",RAND_MAX);
+}
