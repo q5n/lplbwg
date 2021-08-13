b53c34790a56d7259d8150ac1501297d164bba6e
Quentin Liu <quentnliu@gmail.com>
2021-08-14 00:28:27 +0800

    modify wide char convert bug

--- C/1-basic-grammar/src/15_18_wide_chars.c
+++ C/1-basic-grammar/src/15_18_wide_chars.c
@@ -35,14 +35,26 @@ static wchar_t* moreBytesToWideChars(const char *moreBytes){
 
 
 static char* wideCharsToMoreBytes(const wchar_t *wideChars){
-    size_t size=wcslen(wideChars)*4+1;//当前多字节字符集为utf8,字节可变范围(1-4),取最大，乘以4
+    size_t wc_size=wcslen(wideChars);
+    size_t size=wc_size*4+1;//当前多字节字符集为utf8,字节可变范围(1-4),取最大，乘以4
     printf("size=%zd\n",size);
     char* dest=(char*) malloc(size*sizeof(char));
     mbstate_t state;
-    mbrlen(NULL,0,&state);    /* initialize state */
-    size_t len=wcsrtombs(dest,&wideChars,size,&state);//多字节字符串 转 宽字符串
-    printf("len=%zd\n",len);
-    dest[len]='\0';
+    char* tmpDest=dest; 
+    char16_t c16=u'\0';
+    /*若 c16 是空宽字符 u'\0' ，则存储空字节，前附恢复到初始迁移状态所需的任何迁移序列，
+    并更新 state 以表示初始迁移状态。*/
+    size_t mbsLen=c16rtomb(tmpDest,c16,&state);
+    //printf("%04x(%zd) ",c16,mbsLen);
+    for(size_t i=0;i<wc_size;i++){
+        c16=(char16_t)wideChars[i];
+        mbsLen=c16rtomb(tmpDest,c16,&state);
+        if(mbsLen>(size_t)0){
+            tmpDest+=mbsLen;
+        }
+        //printf("%04x(%zd) ",c16,mbsLen);
+    }
+    *tmpDest='\0';
     return dest;
 }
 
@@ -61,24 +73,18 @@ int main(void){
     free(wcStr);
     printf("--------\n");
 
-    wchar_t *widecStr=L"abc你好，世界！";//这里会把‘多字节字符’的每个字符转成宽字符，所以下面会乱码
+    wchar_t *widecStr=L"123abc你好，世界！";//这里会把‘多字节字符’的每个字符转成宽字符，所以下面会乱码
     size_t wc_size=wcslen(widecStr);
     wprintf(L""LINE": wcslen(widecStr)=%zd\n",wc_size);
     wprintf(L""LINE": %ls\n",widecStr); //这里乱码，
-    for(size_t i=0;i<=wc_size;i++){
-        printf("%04lx ",widecStr[i]);
-    }
-    printf("\n");
     
     char *moreBytes=wideCharsToMoreBytes(widecStr);//宽字符串 转 多字节字符串
-
+    printf("\n");
     if(moreBytes!=NULL){
+        printf(LINE": strlen(moreBytes)=%zd\n",strlen(moreBytes)); 
         printf(LINE": %s\n",moreBytes); 
         free(moreBytes);
     }
-    
-
-    printf("~~~\n");
     return 0;
 }
 
